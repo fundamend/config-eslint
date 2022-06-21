8acb46973a0c968da187f1370d549a4ac58e15fb
- <gitkraken@mail.decouplr.com>
2022-06-21 22:39:22 +0200

    test: switch to vitest

--- src/index.cjs
+++ src/index.cjs
@@ -2,14 +2,12 @@ module.exports = {
 	env: {
 		browser: true,
 		es6: true,
-		node: true,
-		jest: true
+		node: true
 	},
-	extends: ['eslint:recommended', 'plugin:jest/recommended', 'prettier'],
+	extends: ['eslint:recommended', 'prettier'],
 	parserOptions: {
 		ecmaVersion: 2018,
 		sourceType: 'module'
 	},
-	plugins: ['jest'],
 	rules: { strict: ['error', 'global'], 'no-console': 2 }
 };
