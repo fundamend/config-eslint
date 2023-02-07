acde0a106ab80c026a9cf5c9ac590cb04c5b67d0
- <gitkraken@mail.decouplr.com>
2023-02-07 22:16:53 +0100

    chore: upgrade eslint

--- src/index.cjs
+++ src/index.cjs
@@ -1,12 +1,11 @@
 module.exports = {
 	env: {
 		browser: true,
-		es6: true,
+		es2022: true,
 		node: true
 	},
 	extends: ['eslint:recommended', 'prettier'],
 	parserOptions: {
-		ecmaVersion: 2018,
 		sourceType: 'module'
 	},
 	rules: { strict: ['error', 'global'], 'no-console': 2 }
