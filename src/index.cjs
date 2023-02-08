63621bd3e17c269424cd73cd5c7a18606e6076a3
- <gitkraken@mail.decouplr.com>
2023-02-08 20:17:57 +0100

    refactor: pull eslint and prettier plugins into base configs

--- src/index.cjs
+++ src/index.cjs
@@ -4,9 +4,15 @@ module.exports = {
 		es2022: true,
 		node: true
 	},
-	extends: ['eslint:recommended', 'prettier'],
+	extends: ['eslint:recommended', 'prettier', 'plugin:astro/recommended'],
+	overrides: [
+		{ files: ['**/*.svelte'], processor: 'svelte3/svelte3' },
+		{ files: ['**/*.astro'], parser: 'astro-eslint-parser' }
+	],
 	parserOptions: {
+		ecmaVersion: 'latest',
 		sourceType: 'module'
 	},
+	plugins: ['svelte3'],
 	rules: { strict: ['error', 'global'], 'no-console': 2 }
 };
